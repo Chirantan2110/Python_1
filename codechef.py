97dc5d77b00cb3cd90c0fb33f3e5a0785e434f52
Chirantan2110 <chirmanna21@gmail.com>
2025-09-08 02:43:41 +0530

    Update codechef.py

--- codechef.py
+++ codechef.py
@@ -25,3 +25,21 @@ elif(a<0):
     print("Negative")
 else:
     print("Zero")
+
+
+
+""""
+Grades of Student
+Write a program to print the grades of a student based on the marks he/she has obtained provided as input. The student is graded A if marks are greater than 90, B if marks are greater than 70, C if greater than or equal to 40, else F.
+"""
+x=int(input())
+if(x>90):
+    print("A")
+elif(x>70):
+    print("B")
+elif(x>=40):
+    print("C")
+else:
+    print("F")
+
+
