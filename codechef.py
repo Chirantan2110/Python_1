53b28e97aa29111049a92f62ffa90778ad5f3909
Chirantan2110 <chirmanna21@gmail.com>
2025-09-14 23:59:15 +0530

    Update codechef.py

--- codechef.py
+++ codechef.py
@@ -113,6 +113,17 @@ for i in range(0,t):
 
 #EOF
 
+"""
+Given an array of ints length 3, return the sum of all the elements.
+"""
+
+def sum3(nums):
+  sum=0
+  for i in nums:
+    sum+=i
+  return sum  
+
+
 
 
 
